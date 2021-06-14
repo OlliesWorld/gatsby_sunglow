eabf9f426f90d1411bf3d404d2705b7b3d8882ed
Roni <rtlockwoodwork@gmail.com>
2021-06-13 19:37:37 -0600

    url

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
       options: {
         url:
         // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
-          process.env.WPGRAPHQL_URL,
+          `https://gatsbysunglow-89f60b.ingress-erytho.easywp.com/graphql`,
           
         schema: {
           //Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
