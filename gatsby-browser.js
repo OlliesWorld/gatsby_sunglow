f462ea6670bb1ec4ad20c5377ffa1aa7af08dcb9
Roni <rtlockwoodwork@gmail.com>
2021-06-22 15:46:18 -0600

    hide browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+// gatsby-node.js 
+// const path = require(`path`)
+
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   return graphql(`
+//     {
+//       allWpPost(sort: { fields: [date] }) {
+//         nodes {
+//           title
+//           excerpt
+//           content
+//           slug
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     console.log(JSON.stringify(result, null, 4))
+//     process.exit()
+//   })
+// }
