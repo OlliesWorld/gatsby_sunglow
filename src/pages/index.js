769a45457a8c959f3d829d33e3cc77d3a51fc52b
Roni <rtlockwoodwork@gmail.com>
2021-06-22 16:05:24 -0600

    all wp gone

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,28 @@
 
 import React from "react";
 // import { StaticImage } from "gatsby-plugin-image";
-import {useStaticQuery, graphql, Link } from 'gatsby'
+// import {useStaticQuery, graphql, Link } from 'gatsby'
+import {Link } from 'gatsby'
  import Header from '../components/Header'
 // import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 
 const HomePage = () => {
-  const data = useStaticQuery(graphql `
-    query MyQuery {
-          allWpPost {
-            nodes {
-              id
-              title
-              uri
-              excerpt
-            }
-          }
-        }
+  // const data = useStaticQuery(graphql `
+  //   query MyQuery {
+  //         allWpPost {
+  //           nodes {
+  //             id
+  //             title
+  //             uri
+  //             excerpt
+  //           }
+  //         }
+  //       }
 
-  `)
+  // `)
   // console.log(data)
-  const {allWpPost} = data
+  // const {allWpPost} = data
   return (
     <main >
       <title>Home Page</title>
@@ -29,7 +30,7 @@ const HomePage = () => {
       <Hero />
         <h1>SunGlow Films Durango</h1>
         
-          {allWpPost.nodes.map( ({id, title, excerpt,uri}) => (
+          {/* {allWpPost.nodes.map( ({id, title, excerpt,uri}) => (
             <div className="box" key={id}>
             <h2>{title}</h2>
            
@@ -37,18 +38,18 @@ const HomePage = () => {
             
             <Link to={uri}>Read More</Link>
           </div>
-          )) }
+          )) } */}
           
           <div className="box">
             <h2 >Goodby Mars</h2>
             
-            <span dangerouslySetInnerHTML={{__html:'<p>This is a 2nd excerpt..</p>'}} />
+            {/* <span dangerouslySetInnerHTML={{__html:'<p>This is a 2nd excerpt..</p>'}} /> */}
            
             <Link to='#'>Read More</Link>
        </div>
     
     </main>
   );
-};
+ };
 
-export default HomePage;
+export default HomePage
--- src/templates/WpPage.js
+++ src/templates/WpPage.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import {graphql, Link } from 'gatsby'
+// import React from 'react'
+// import {graphql, Link } from 'gatsby'
 
-const WpPage = ({data}) => {
-    // const {WpPage} = data
-    return (
-        <>
-        <div className="container">
-            <h1>{data.title}</h1>
-            <div dangerouslySetInnerHTML={{__html: data.content}} />
-            <Link to="/">Back to home</Link>
-        </div>
-        </>
-    )
-}
-export default WpPage
+// const WpPage = ({data}) => {
+//     // const {WpPage} = data
+//     return (
+//         <>
+//         <div className="container">
+//             <h1>{data.title}</h1>
+//             <div dangerouslySetInnerHTML={{__html: data.content}} />
+//             <Link to="/">Back to home</Link>
+//         </div>
+//         </>
+//     )
+// }
+// export default WpPage
 
-export const query = graphql`
-    query PageById($id: String){
-         wpPage(id: {eq: $id}) {
-             __typename
-            id
-            uri
-            title
-            content
+// export const query = graphql`
+//     query PageById($id: String){
+//          wpPage(id: {eq: $id}) {
+//              __typename
+//             id
+//             uri
+//             title
+//             content
             
-  }
-}
+//   }
+// }
 
-`
+// `
